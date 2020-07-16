0414a45cd73eef3ef24d84b38b74a68b7fd260f7
Aleksandr Suvorov <myhackband@gmail.com>
2020-07-16 16:48:46 +0700

    Added the cart app:
    - Added a shopping cart (shopping cart data is stored in sessions)
     You can add and remove products from the shopping cart. Updating products in the shopping cart is currently in development.
    
    Errors:
     - Incorrect updating of products in the shopping cart

--- myshop/settings.py
+++ myshop/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
 
     # My Apps
     'shop.apps.ShopConfig',
+    'cart.apps.CartConfig',
 ]
 
 MIDDLEWARE = [
@@ -66,6 +67,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'cart.context_processors.cart',
             ],
         },
     },
@@ -124,4 +126,6 @@ USE_TZ = True
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media/')
+CART_SESSION_ID = 'cart'
+
 
