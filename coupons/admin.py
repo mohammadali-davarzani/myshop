5f9d64151573efa36d78fa21e2c623670aced097
Aleksandr Suvorov <myhackband@gmail.com>
2020-07-26 03:49:24 +0700

    Implemented a coupon system

--- coupons/admin.py
+++ coupons/admin.py
@@ -4,8 +4,8 @@ from .models import Coupon
 
 
 class CouponAdmin(admin.ModelAdmin):
-    list_display = ['code', 'valid_form', 'valid_to', 'discount', 'active']
-    list_filter = ['active', 'valid_form', 'valid_to']
+    list_display = ['code', 'valid_from', 'valid_to', 'discount', 'active']
+    list_filter = ['active', 'valid_from', 'valid_to']
     search_fields = ['code']
 
 
--- coupons/models.py
+++ coupons/models.py
@@ -4,7 +4,7 @@ from django.core.validators import MinValueValidator, MaxValueValidator
 
 class Coupon(models.Model):
     code = models.CharField(max_length=50, unique=True)
-    valid_form = models.DateTimeField()
+    valid_from = models.DateTimeField()
     valid_to = models.DateTimeField()
     discount = models.IntegerField()
     validators = [MinValueValidator(0), MaxValueValidator(100)]
